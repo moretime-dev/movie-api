a02e14c12318a8321df5096d297eb54d607bf26c
moretime-dev <moretimeproductions@googlemail.com>
2022-02-02 10:36:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+export default function App() {
+  const [movies, setMovies] = useState([]);
+
+  console.log(movies);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const results = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=0b7131c882252f4d2e4fbdb70636cf42&page=5"
+      );
+
+      const movies = await results.json();
+
+      setMovies(movies);
+    };
+
+    fetchMovies();
+  }, []);
+
+  return <div className="App"></div>;
 }
-
-export default App;
